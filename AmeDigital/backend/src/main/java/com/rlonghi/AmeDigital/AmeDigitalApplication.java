c36bcc3a070880fd71e0d2d429902859c82d667a
Rodrigo Longhi <rfslonghi@gmail.com>
2021-05-13 21:28:53 -0300

    Project created

---
+++ AmeDigital/backend/src/main/java/com/rlonghi/AmeDigital/AmeDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.rlonghi.AmeDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmeDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmeDigitalApplication.class, args);
+	}
+
+}
---
+++ AmeDigital/backend/src/test/java/com/rlonghi/AmeDigital/AmeDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlonghi.AmeDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmeDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
